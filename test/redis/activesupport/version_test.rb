4bdfbbc913cd861419cc480e7ef20e9a4fb49681
Balasankar C <balasankarc@autistici.org>
2015-01-17 16:02:43 +0530

    Fix patch issues

--- test/redis/activesupport/version_test.rb
+++ test/redis/activesupport/version_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
-require 'version'
 describe Redis::ActiveSupport::VERSION do
   it 'returns current version' do
- #   Redis::ActiveSupport::VERSION.must_equal '4.0.0'
+    Redis::ActiveSupport::VERSION.must_equal '4.0.0'
   end
 end
